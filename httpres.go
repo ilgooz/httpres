c39114be1b423a84b051e6260a605252763da334
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-09-16 15:54:50 +0300

    fix names

--- httpres.go
+++ httpres.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func Response(w http.ResponseWriter, status int, data interface{}) {
+func Json(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 	err := json.NewEncoder(w).Encode(data)
@@ -15,6 +15,6 @@ func Response(w http.ResponseWriter, status int, data interface{}) {
 	}
 }
 
-func ResponseFile(w http.ResponseWriter, r *http.Request, path string) {
+func File(w http.ResponseWriter, r *http.Request, path string) {
 	http.ServeFile(w, r, path)
 }
